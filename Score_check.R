a2a55b886312c478bf32046103a4aa916ac9d1e8
Yu Dong <yu.dong.2017@marshall.usc.edu>
2017-05-03 00:11:07 -0700

    update

--- Score_check.R
+++ Score_check.R
@@ -1,29 +1,48 @@
 library(dplyr)
 
-load("data_type_abcd_allwindow.Rdata")
+setwd("~/Desktop/DSO 562/Project3")
+
+load("all_abcd_updated.rda")
 scores = read.csv("Fraud Scores.csv")
 
 all_abcd = all_abcd[all_abcd$date > "2010-01-28",]
 all_abcd$fraud[is.na(all_abcd$fraud)] = 0
 
-scores$final = scores$RF + 0.8 * scores$NN + 0.6 * scores$SVM + 0.4 * scores$LR + 0.2 * scores$NB
+#scores$final = scores$RF + 0.8 * scores$NN + 0.6 * scores$SVM + 0.4 * scores$LR + 0.2 * scores$NB
 
-score_comparison = data.frame(fraud = all_abcd$fraud, score = scores$final)
-score_final = data.frame(all_abcd[,1:10], score = scores$final)
-write.csv(score_final, file = "Fraud Scores Final.csv")
+score_comparison = cbind(all_abcd[,c(1,10)], scores[,2:6])
+#score_final = data.frame(all_abcd[,1:10], score = scores$final)
+#write.csv(score_final, file = "Fraud Scores Final.csv")
+score_comparison$avg_score = (score_comparison$RF + score_comparison$NN)/2
+score_comparison$max_score = ifelse(score_comparison$RF>score_comparison$NN, score_comparison$RF, score_comparison$NN)
 
-top_score_record = scores %>%
-    arrange(-final) %>%
-    top_n(100, final) %>%
-    select(X)
+top_score_record = score_comparison %>%
+    arrange(-avg_score) %>%
+    top_n(100, avg_score)
 
-top_records = all_abcd %>%
-    filter(recordnum %in% top_score_record$X)
+sum(top_score_record$fraud)
 
-sum(top_records$fraud)
+top_score_record = score_comparison %>%
+    arrange(-avg_score) %>%
+    top_n(300, avg_score)
 
-top_100 = score_comparison %>%
-    arrange(-score) %>%
-    top_n(300, score)
+sum(top_score_record$fraud)
 
-sum(top_100$fraud)
+
+top_score_record = score_comparison %>%
+    arrange(-max_score) %>%
+    top_n(100, max_score)
+
+sum(top_score_record$fraud)
+
+top_score_record = score_comparison %>%
+    arrange(-max_score) %>%
+    top_n(300, max_score)
+
+sum(top_score_record$fraud)
+
+top_score_record = score_comparison %>%
+    arrange(-max_score) %>%
+    top_n(889, max_score)
+
+sum(top_score_record$fraud)
